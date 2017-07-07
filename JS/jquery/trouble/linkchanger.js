d757436bf89178f0c6bd47720d5aa343746b6926
ChanChan <chnajuha@hotmail.com>
2017-07-07 19:45:38 +0800

    1.1.9

---
+++ JS/jquery/trouble/linkchanger.js
@@ -0,0 +1,88 @@
+// JavaScript Document
+
+$("#current").click(function(e) {
+	$("#box1").slideDown(1000);
+	$("#box2").slideDown(1000);
+	$("#box3").slideDown(1000);
+	$("#box4").slideDown(1000);
+	
+});
+
+$("#Back").click(function(e) {
+	$("#box1").slideDown(1000);
+	$("#box2").slideDown(1000);
+	$("#box3").slideDown(1000);
+	$("#box4").slideDown(1000);
+	
+});
+
+$("#clickFTU").click(function(e) {
+	e.preventDefault();
+	$("#box1").slideDown(1000);
+	$("#box2").slideUp(1000);
+	$("#box3").slideUp(1000);
+	$("#box4").slideUp(1000);
+	
+});
+
+$("#clickFTUlink").click(function(e) {
+	e.preventDefault();
+	$("#box1").slideDown(1000);
+	$("#box2").slideUp(1000);
+	$("#box3").slideUp(1000);
+	$("#box4").slideUp(1000);
+});
+
+$("#clickVirus").click(function(e) {
+	e.preventDefault();
+	$("#box1").slideUp(1000);
+	$("#box2").slideDown(1000);
+	$("#box3").slideUp(1000);
+	$("#box4").slideUp(1000);
+});
+
+$("#clickViruslink").click(function(e) {
+	e.preventDefault();
+	$("#box1").slideUp(1000);
+	$("#box2").slideDown(1000);
+	$("#box3").slideUp(1000);
+	$("#box4").slideUp(1000);
+	
+});
+
+$("#clickMalware").click(function(e) {
+	e.preventDefault();
+	$("#box1").slideUp(1000);
+	$("#box2").slideUp(1000);
+	$("#box3").slideDown(1000);
+	$("#box4").slideUp(1000);
+	
+});
+
+$("#clickMalwarelink").click(function(e) {
+	e.preventDefault();
+	$("#box1").slideUp(1000);
+	$("#box2").slideUp(1000);
+	$("#box3").slideDown(1000);
+	$("#box4").slideUp(1000);
+	
+});
+
+$("#clickTorjan").click(function(e) {
+	e.preventDefault();
+	$("#box1").slideUp(1000);
+	$("#box2").slideUp(1000);
+	$("#box3").slideUp(1000);
+	$("#box4").slideDown(1000);
+	
+});
+
+$("#clickTorjanlink").click(function(e) {
+	e.preventDefault();
+	$("#box1").slideUp(1000);
+	$("#box2").slideUp(1000);
+	$("#box3").slideUp(1000);
+	$("#box4").slideDown(1000);
+	
+});
+
