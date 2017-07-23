6cc767535d32caffc971774ceffa8da1230601dc
alanx0401 <chienlunhuang@gmail.com>
2017-07-23 20:38:52 +0800

    All Freatures done!
    left with beautification and matching to template

--- JS/formprocessing.js
+++ JS/formprocessing.js
@@ -1,10 +1,15 @@
+var news= [];
+
 $('#ready').click(function(){
-var title = $("#title").val();
-var text = $("#textarea").val();
-var inputName=document.getElementById("name").value;
-var inputContact=document.getElementById("contact").value;
-var obj={"name":inputName, "contact":inputContact};
-localStorage["theContact"]=JSON.stringify(obj);	
+	if (news.length > 3){
+	news.pop();
+}
+	var title = $("#title").val();
+	var text = $("#textarea").val();
+	var obj={"title":title, "text":text};
+	news.push(obj);
+	console.log(news);
+	localStorage["AllNews"]=JSON.stringify(news);	
 });
 
 
