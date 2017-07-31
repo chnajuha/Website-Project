31aac8a74be3a3c1a37ab5a4d1989036544df189
wycliff99 <wycliff_lee@hotmail.com>
2017-07-31 15:07:59 +0800

    Add files via upload

---
+++ JS/seminar.js
@@ -0,0 +1,19 @@
+
+
+function click(){
+	var name=document.getElementById("personname").value;
+	var mail=document.getElementById("personemail").value;
+	var choice=document.getElementById("personchoice").value;
+	var QnS=document.getElementById("questionsuggestion").value;
+	console.log(name)
+	console.log(mail)
+	console.log(choice)
+	console.log(QnS)
+	localStorage["name"]=JSON.stringify(name);
+	localStorage["mail"]=JSON.stringify(mail);
+	localStorage["choice"]=JSON.stringify(choice);
+	localStorage["QnS"]=JSON.stringify(QnS);
+}
+
+
+
