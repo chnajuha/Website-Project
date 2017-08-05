43336fbaaf0c396904370f7645594f7eeda0dd39
alanx0401 <chienlunhuang@gmail.com>
2017-08-05 19:13:53 +0800

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,46 @@
+//JavaScript Document
+
+//Problem: When the user click on the image, it goes to a dead end.
+//Solution: Create an overlay with large Image and caption like Ligthbox.
+
+//Create overlay jquery object
+var overlay = $("<div id='overlay'></div>");
+var image = $("<img>");
+var breakline = $("<br>")
+var breakline1 = $("<br>")
+var caption = $("<a id='link'></a>");
+
+
+//Append the overlay to the html page.
+$("body").append(overlay);
+overlay.append(image);
+overlay.append(breakline);
+overlay.append(breakline1)
+overlay.append(caption);
+
+overlay.hide();
+image.hide(1000);
+
+//Capture the click event on a link to an image
+$(".imagegallery a").click(function(evt) {
+	
+	evt.preventDefault(); //To stop the link.
+	
+	var imgLoc = $(this).attr("href");
+		image.attr("src",imgLoc);
+		
+	var captiontxt = "Cartoon Network" //$(this).children("img").attr("alt");
+		caption.attr("href", $(this).children("img").attr("alt"));
+		caption.text(captiontxt);
+		
+	overlay.fadeIn();
+	image.show(1000);
+});
+
+//When overlay is clicked, hide the overlay.
+$("#overlay img").click(function() {
+	$("#overlay").fadeOut(1000);
+	image.hide(1000);
+});
+
+
